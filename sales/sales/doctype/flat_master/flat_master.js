418330dadde7963417274a4ae9c90898ffc47dc8
Ubuntu <azureuser@erpnext5ubu1.erpnext5ubu1.g7.internal.cloudapp.net>
2015-10-22 16:34:06 +0000

    V5

--- sales/sales/doctype/flat_master/flat_master.js
+++ sales/sales/doctype/flat_master/flat_master.js
@@ -2,3 +2,24 @@ cur_frm.add_fetch("flat_type","flat_type","type")
 cur_frm.add_fetch("flat_type","plc","plc_rate")
 cur_frm.add_fetch("floor_link","floor","floor")
 cur_frm.add_fetch("floor_link","frc","frc_rate")
+frappe.ui.form.on("Flat Master","flat_no",function(frm)
+{
+	frm.set_value("flat_name",frm.doc.flat_no);
+});
+
+
+frappe.ui.form.on("Flat Master", "save", function(frm) {
+    frappe.msgprint("JS")
+    if(frm.doc.flat_no) {
+        cur_frm.call({
+            method: "insertItem",
+            args: {
+            },
+            callback: function(r, rt) {
+                if(r.message) {
+                    
+                }
+            }
+        });
+    }
+})
--- sales/sales/doctype/flat_master/flat_master.py
+++ sales/sales/doctype/flat_master/flat_master.py
@@ -7,4 +7,38 @@ import frappe
 from frappe.model.document import Document
 
 class FlatMaster(Document):
-	pass
+	
+
+
+	def insertItem(self):
+		frappe.msgprint("insertItem")
+		im=frappe.get_doc({      
+		"doctype": "Item",
+		"item_code":self.flat_no,
+		"item_name":self.flat_no,
+		"item_group":"All Item Group",
+		"default_warehouse":"Stores-TGR",
+
+		}).insert
+	
+
+
+
+
+#Insert Same Item in to the Item master
+@frappe.whitelist(allow_guest=True)
+def saveItem(self,method):
+	frappe.msgprint("Start")
+	si =  frappe.get_doc({      
+			"doctype": "Item",
+				"item_name":self.flat_no,
+				"item_code":self.flat_no,
+				"description":self.flat_no,
+				"item_group":"All Item Groups",
+				"default_warehouse":"Stores - ST",
+    	}).insert()
+	frappe.msgprint("Over")
+	#im.save()
+	#im.submit()
+
+
